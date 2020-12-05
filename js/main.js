1701ad158e34a81ea957fea167f20a375b2742fa
YassineCherkaoui <yassin.cherkaoui2000@gmail.com>
2020-12-05 22:32:00 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Yassine Cherkaoui.", "Designer.", "Developer.", "Freelancer.", "20yearsold."],
+      strings: ["Yassine Cherkaoui.", "Designer.", "Full Stack Developer.", "Freelancer.", "21yearsold."],
       typeSpeed: 100,
       loop: true,
     });
